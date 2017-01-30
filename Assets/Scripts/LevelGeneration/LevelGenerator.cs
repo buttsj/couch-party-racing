bb1d039776a1b1925e301cdd7579acf58fe30237
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-01-30 17:34:01 -0500

    Initial XML Level Reading

---
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    private TileContainer tileCollection = new TileContainer();
+    private string levelName;
+    
+    public LevelGenerator(string levelName) {
+        this.levelName = levelName;
+    }
+
+    public void Start() {
+        GenerateLevel();
+    }
+
+    public void GenerateLevel() {
+        tileCollection = TileContainer.Load(Path.Combine(Application.dataPath, "tiles.xml"));
+        
+        foreach (var item in tileCollection.tileList) {
+            Debug.Log(item.Size.X);
+            Debug.Log(item.Size.Y);
+            Debug.Log(item.Size.Z);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/LevelGeneration/TileContainer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("TileCollection")]
+public class TileContainer {
+
+    [XmlArray("Tiles"), XmlArrayItem("Tile")]
+    public List<Tile> tileList = new List<Tile>();
+
+    private string levelName;
+
+    public void Save(string path) {
+        var serializer = new XmlSerializer(typeof(TileContainer));
+        using (var stream = new FileStream(path, FileMode.Create)) {
+            serializer.Serialize(stream, this);
+        }
+    }
+
+    public static TileContainer Load(string path) {
+        var serializer = new XmlSerializer(typeof(TileContainer));
+        using (var stream = new FileStream(path, FileMode.Open)) {
+            return serializer.Deserialize(stream) as TileContainer;
+        }
+    }
+
+    //Loads the xml directly from the given string. Useful in combination with www.text.
+    public static TileContainer LoadFromText(string text) {
+        var serializer = new XmlSerializer(typeof(TileContainer));
+        return serializer.Deserialize(new StringReader(text)) as TileContainer;
+    }
+}
