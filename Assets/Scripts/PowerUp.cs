9e8b12bbd6cc6531e6bcf3a5279c035ede2a36d6
John (Jack) Butts <butts.102@osu.edu>
2017-01-31 16:59:40 -0500

    beginning to add power up

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        transform.Rotate(Vector3.right, 2.0f);
+    }
+}
