ce9fc7ea859c3768a9fb85dc006d31b055dacf07
Jack Butts <butts.102@osu.edu>
2017-02-01 11:15:42 -0500

    changed minimap to Ortho view, red dot above kart now

---
+++ Assets/Scripts/FollowKart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowKart : MonoBehaviour {
+
+    public GameObject kart;
+
+    void FixedUpdate ()
+    {
+        transform.position = new Vector3(kart.transform.position.x, transform.position.y, kart.transform.position.z);
+    }
+}
