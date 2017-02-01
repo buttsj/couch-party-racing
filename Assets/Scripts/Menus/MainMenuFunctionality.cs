7b008f742469f854bdc468310f84563b34ef8f49
John <cramer240@gmail.com>
2017-01-31 22:45:32 -0500

    Created Main Menu Layout
    Created the layout for the main menu, and added layout for transitioning
    between main menu, and other game scenes.

---
+++ Assets/Scripts/Menus/MainMenuFunctionality.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctionality : MonoBehaviour {
+
+    public Canvas quitMenu;
+
+    public Button raceMode;
+    public Button trackBuilderMode;
+    public Button asymmetricMode;
+    public Button playgroundMode;
+    public Button hotPotatoMode;
+    public Button settings;
+    public Button exit;
+
+    void Start () {
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        quitMenu.enabled = false;
+
+        raceMode = raceMode.GetComponent<Button>();
+        trackBuilderMode = trackBuilderMode.GetComponent<Button>();
+        asymmetricMode = asymmetricMode.GetComponent<Button>();
+        playgroundMode = playgroundMode.GetComponent<Button>();
+        hotPotatoMode = hotPotatoMode.GetComponent<Button>();
+        settings = settings.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+    }
+
+    public void settingsPress()
+    {
+        // Add Settings
+    }
+
+    public void exitPress()
+    {
+        quitMenu.enabled = true;
+
+        raceMode.enabled = false;
+        trackBuilderMode.enabled = false;
+        asymmetricMode.enabled = false;
+        playgroundMode.enabled = false;
+        hotPotatoMode.enabled = false;
+        settings.enabled = false;
+        exit.enabled = false;
+    }
+
+    public void noQuitPress()
+    {
+        quitMenu.enabled = false;
+
+        raceMode.enabled = true;
+        trackBuilderMode.enabled = true;
+        asymmetricMode.enabled = true;
+        playgroundMode.enabled = true;
+        hotPotatoMode.enabled = true;
+        settings.enabled = true;
+        exit.enabled = true;
+    }
+
+    public void yesQuitPress()
+    {
+        Application.Quit();
+    }
+
+    public void raceModePress()
+    {
+        //SceneManager.LoadScene("RaceModeSelection");
+        SceneManager.LoadScene("Temp_Scene");
+    }
+
+    public void trackBuilderModePress()
+    {
+        //SceneManager.LoadScene("BuilderSelection");
+    }
+
+    public void asymmetricModePress()
+    {
+        //SceneManager.LoadScene("AsymmetricSelection");
+    }
+
+    public void playgroundModePress()
+    {
+        //SceneManager.LoadScene("PlaygroundSelection");
+    }
+
+    public void hotPotatoModePress()
+    {
+        //SceneManager.LoadScene("HotPotatoSelection");
+    }
+
+}
