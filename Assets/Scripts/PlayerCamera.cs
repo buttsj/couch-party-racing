24404f3f0a623e75e5744c062d53087a0043c61a
powerskyle26 <powers.411@osu.edu>
2017-02-01 11:22:22 -0500

    Updated kart prefab to no longer require main camera to follow it, smoothed camera rotation.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform player;
+    public float followDistance;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start() {
+        offset = new Vector3(0, 7, -followDistance);
+    }
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.TransformPoint(offset);
+        transform.localEulerAngles = new Vector3(player.localEulerAngles.x, player.localEulerAngles.y, 0);
+        
+    }
+}
