fb59dd3487ebe189052baaf443a7a33bd688039d
John (Jack) Butts <butts.102@osu.edu>
2017-02-02 22:26:50 -0500

    added video in main menu

---
+++ Assets/Scripts/Input/RecordingCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordingCamera : MonoBehaviour {
+
+    public GameObject leftwheel;
+    public GameObject rightwheel;
+    public GameObject backlwheel;
+    public GameObject backrwheel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        leftwheel.transform.Rotate(Vector3.right, 100.0f);
+        rightwheel.transform.Rotate(Vector3.right, 100.0f);
+        backlwheel.transform.Rotate(Vector3.right, 100.0f);
+        backrwheel.transform.Rotate(Vector3.right, 100.0f);
+        //transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), 10.0f * Time.deltaTime);
+    }
+
+    void FixedUpdate ()
+    {
+    }
+}
---
+++ Assets/Scripts/Menus/VideoPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayer : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	// Use this for initialization
+	void Start () {
+        movie.loop = true;
+        movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
