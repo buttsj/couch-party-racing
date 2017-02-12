02e9257bc9508ef6f13cfe3baf175a57051d7807
John (Jack) Butts <butts.102@osu.edu>
2017-02-12 11:36:25 -0500

    starting more work on SpudRun

--- Assets/Scripts/SpudGame/SpudScript.cs
+++ Assets/Scripts/SpudGame/SpudScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class SpudScript : MonoBehaviour {
 
+    private float timer;
+    private bool tagged;
+    public float TimeRemaining { get { return timer; } set { timer = value; } }
+    public bool IsTagged { get { return tagged; } set { tagged = value; } }
+
 	// Use this for initialization
 	void Start () {
-		
+        timer = 60.0f;
+        tagged = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (tagged)
+        {
+            timer = timer - Time.deltaTime;
+        }
+        if (timer < 0.0f)
+        {
+            // game over
+        }
 	}
 }
