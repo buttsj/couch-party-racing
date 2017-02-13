ab75d639bb76e68fb2346bb2e4f88febc9ab1245
John (Jack) Butts <butts.102@osu.edu>
2017-02-13 11:23:11 -0500

    i think the Spud timer works better now

--- Assets/Scripts/HUD Logic/SpudHUDManager.cs
+++ Assets/Scripts/HUD Logic/SpudHUDManager.cs
@@ -13,6 +13,10 @@ public class SpudHUDManager : MonoBehaviour {
     List<float> seconds;
     List<int> minutes;
 
+    public Text timeRemainingText;
+    List<float> secondsRemain;
+    List<int> minutesRemain;
+
     // Use this for initialization
     void Start()
     {
@@ -20,6 +24,11 @@ public class SpudHUDManager : MonoBehaviour {
         minutes = new List<int>();
         seconds.Add(0f);
         minutes.Add(0);
+
+        secondsRemain = new List<float>();
+        minutesRemain = new List<int>();
+        secondsRemain.Add(60f);
+        minutesRemain.Add(0);
     }
 
     // Update is called once per frame
@@ -28,6 +37,7 @@ public class SpudHUDManager : MonoBehaviour {
         boost = kart.GetComponent<Kart>().Boost;
         boostText.text = ((int)boost).ToString();
         poweruptype = kart.GetComponent<Kart>().Powerup;
+
         if (potato.GetComponent<SpudScript>().IsTagged)
         {
             UpdateTimerUI(kart, 0);
@@ -35,35 +45,46 @@ public class SpudHUDManager : MonoBehaviour {
         
     }
 
+
     public void UpdateTimerUI(GameObject kart, int kartNumber)
     {
         string minuteText;
         string secondsText;
+        string minuteRemainText;
+        string secondsRemainText;
         seconds[kartNumber] += Time.deltaTime;
+        secondsRemain[kartNumber] -= Time.deltaTime;
 
         if (seconds[kartNumber] < 10)
         {
             secondsText = "0" + seconds[kartNumber].ToString("F2");
+            secondsRemainText = secondsRemain[kartNumber].ToString("F2");
         }
         else
         {
             secondsText = seconds[kartNumber].ToString("F2");
+            secondsRemainText = secondsRemain[kartNumber].ToString("F2");
         }
 
         if (minutes[kartNumber] < 10)
         {
             minuteText = "0" + minutes[kartNumber];
+            minuteRemainText = "0" + minutesRemain[kartNumber];
         }
         else
         {
             minuteText = minutes[kartNumber].ToString();
+            minuteRemainText = minutesRemain[kartNumber].ToString();
         }
         string timeDisplay = minuteText + ":" + secondsText;
+        string timeRemainDisplay = minuteRemainText + ":" + secondsRemainText;
 
         if (kartNumber == 0)
         {
             kart.GetComponent<Kart>().TimeText = timeDisplay;
+            potato.GetComponent<SpudScript>().TimeRemaining = timeRemainDisplay;
             timerText.text = timeDisplay;
+            timeRemainingText.text = timeRemainDisplay;
         }
         else
         {
@@ -74,6 +95,8 @@ public class SpudHUDManager : MonoBehaviour {
         {
             minutes[kartNumber]++;
             seconds[kartNumber] = 0;
+            minutesRemain[kartNumber]++;
+            secondsRemain[kartNumber] = 0;
         }
     }
 }
--- Assets/Scripts/SpudGame/SpudScript.cs
+++ Assets/Scripts/SpudGame/SpudScript.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class SpudScript : MonoBehaviour {
 
-    private float timer;
+    private string timer;
     private bool tagged;
     private GameObject holder;
 
-    public float TimeRemaining { get { return timer; } set { timer = value; } }
+    public string TimeRemaining { get { return timer; } set { timer = value; } }
     public bool IsTagged { get { return tagged; } set { tagged = value; } }
     public GameObject SpudHolder { get { return holder; } set { holder = value; } }
 
 	// Use this for initialization
 	void Start () {
-        timer = 60.0f;
         tagged = false;
 	}
 	
@@ -22,17 +21,12 @@ public class SpudScript : MonoBehaviour {
 	void Update () {
 		if (tagged)
         {
-            timer = timer - Time.deltaTime;
             transform.position = holder.transform.position;
             transform.rotation = holder.transform.rotation;
         }
         else
         {
             transform.Rotate(Vector3.up, 2.0f);
-        }
-        if (timer < 0.0f)
-        {
-            // game over
         }
 	}
 }
