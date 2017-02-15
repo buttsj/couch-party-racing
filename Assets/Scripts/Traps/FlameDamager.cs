aec9058353a23663822e950123a6113f997633e3
connor-swick <swick.64@osu.edu>
2017-02-14 20:11:08 -0500

    began flame damage functionality

---
+++ Assets/Scripts/Traps/FlameDamager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameDamager : MonoBehaviour {
+
+    ParticleSystem flameEmitter;
+
+	// Use this for initialization
+	void Start () {
+		flameEmitter = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Kart") || flameEmitter.isEmitting)
+        {
+            //need to somehow access kart to damage
+        }
+    }
+}
