ed6d003a357dde602e40e006a92aac9ac365355d
John Cramer <cramer240@gmail.com>
2017-02-19 20:30:53 -0500

    Modified Pause menu to only use SimpleInput

--- Assets/Scripts/Menus/PauseMenuFunctionality.cs
+++ Assets/Scripts/Menus/PauseMenuFunctionality.cs
@@ -1,55 +1,128 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 public class PauseMenuFunctionality : MonoBehaviour {
 
+    private const int NUMBEROFBUTTONS = 2;
+
     public Canvas pauseMenu;
 
-    public Button resume;
-    public Button quit;
+    public Text resumeText;
+    public Text quitText;
 
     private float defaultTimeScale;
 
+    private Text[] buttons;
+    private int currentButton;
+
+    private bool axisEnabled;
+
+    private Color highlight;
+
     void Start () {
-
+        highlight = new Color(255, 255, 0);
         pauseMenu = pauseMenu.GetComponent<Canvas>();
-
-        resume = resume.GetComponent<Button>();
-        quit = quit.GetComponent<Button>();
 
         pauseMenu.enabled = false;
 
         defaultTimeScale = Time.timeScale;
 
+        buttons = new Text[NUMBEROFBUTTONS];
+        buttons[0] = resumeText;
+        buttons[1] = quitText;
+
+        currentButton = 0;
+        resumeText.color = highlight;
+
+        axisEnabled = true;
     }
 	
 	void Update () {
+
         if (!GameObject.Find("RacingEndMenu(Clone)").GetComponent<RacingEndGameMenu>().RaceOver)
         {
             if (SimpleInput.GetButtonDown("Pause", 1) && !pauseMenu.enabled)
             {
                 pauseMenu.enabled = true;
                 Time.timeScale = 0;
-                EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(GameObject.Find("ResumeText"));
+                resumeText.color = highlight;
             }
         }
+
+        scrollMenu();
+
+        buttonPress();
 	}
 
-    public void resumePress()
+    private void buttonPress()
+    {
+        if(pauseMenu.enabled && SimpleInput.GetButtonDown("Bump Kart"))
+        {
+            if(ReferenceEquals(buttons[currentButton], resumeText))
+            {
+                resumePress();
+            }
+            else if(ReferenceEquals(buttons[currentButton], quitText))
+            {
+                quitPress();
+            }
+                
+        }
+    }
+
+    private void resumePress()
     {
         pauseMenu.enabled = false;
         Time.timeScale = defaultTimeScale;
     }
 
-    public void quitPress()
+    private void quitPress()
     {
         pauseMenu.enabled = false;
         Time.timeScale = defaultTimeScale;
         SceneManager.LoadScene("MainMenu");
+    }
+
+    private void scrollMenu()
+    {
+        if (pauseMenu.enabled)
+        {
+            if((SimpleInput.GetAxis("Vertical", 1) > 0 && axisEnabled) || SimpleInput.GetButtonDown("Accelerate"))
+            {
+                axisEnabled = false;
+                currentButton++;
+                if(currentButton >= NUMBEROFBUTTONS)
+                {
+                    currentButton = 0;
+                }
+                colorSelectedButton();
+            }
+            else if((SimpleInput.GetAxis("Vertical", 1) < 0 && axisEnabled) || SimpleInput.GetButtonDown("Reverse"))
+            {
+                axisEnabled = false;
+                currentButton--;
+                if(currentButton < 0)
+                {
+                    currentButton = NUMBEROFBUTTONS - 1;
+                }
+                colorSelectedButton();
+            }
+
+            if(SimpleInput.GetAxis("Vertical", 1) == 0)
+            {
+                axisEnabled = true;
+            }
+        }
+    }
+
+    private void colorSelectedButton()
+    {
+        for(int i = 0; i < NUMBEROFBUTTONS; i++)
+        {
+            buttons[i].color = Color.white;
+        }
+        buttons[currentButton].color = highlight;
     }
 
 }
