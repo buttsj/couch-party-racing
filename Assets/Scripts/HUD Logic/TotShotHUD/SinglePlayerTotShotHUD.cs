d265674c9171bbe7e19adf3df80fcad53560ad42
connor-swick <swick.64@osu.edu>
2017-02-27 23:02:17 -0500

    Worked on some Tot behavior, some single player HUD logic

---
+++ Assets/Scripts/HUD Logic/TotShotHUD/SinglePlayerTotShotHUD.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SinglePlayerTotShotHUD : MonoBehaviour
+{
+
+    private float boost;
+    public GameObject kart;
+    private List<GameObject> aiList;
+    public Text boostText;
+    public Text redScore;
+    public Text blueScore;
+    public Text time;
+    List<float> seconds;
+    List<int> minutes;
+
+    // Use this for initialization
+    void Start()
+    {
+        aiList = new List<GameObject>();
+        seconds = new List<float>();
+        minutes = new List<int>();
+        seconds.Add(0f);
+        minutes.Add(0);
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (player.name.Contains("AI"))
+            {
+                aiList.Add(player);
+                seconds.Add(0f);
+                minutes.Add(0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boost = kart.GetComponent<Kart>().Boost;
+        boostText.text = ((int)boost).ToString();
+    }
+}
---
+++ Assets/Scripts/TotShot/TotScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotScript : MonoBehaviour {
+
+    public GameObject tot;
+    public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+        explosion.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkForGoal();
+        updateExplosionPosition();
+
+    }
+
+    public void checkForGoal()
+    {
+        if (tot.transform.position.z >= 153)
+        {
+            explosion.Play();
+            tot.SetActive(false);
+        }
+        else if (tot.transform.position.z <= -153)
+        {
+            explosion.Play();
+            tot.SetActive(false);
+        }
+    }
+
+    public void updateExplosionPosition()
+    {
+        explosion.transform.position = new Vector3(tot.transform.position.x, tot.transform.position.y, tot.transform.position.z);
+    }
+}
