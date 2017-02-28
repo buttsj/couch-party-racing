0c28f87494461f7c02f0a6c4ecc982301102bdb0
John (Jack) Butts <butts.102@osu.edu>
2017-02-27 20:50:25 -0500

    transition to Level Select & noise for Esc key

--- Assets/Scripts/Menus/GoToPreviousScene.cs
+++ Assets/Scripts/Menus/GoToPreviousScene.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GoToPreviousScene : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-        if (SimpleInput.GetButton("Cancel")) {
-            var prevScene = SceneManager.GetActiveScene().buildIndex - 1;
 
-            if (prevScene == 0) {
-                Destroy(GameObject.Find("SceneGenerator"));
-            }
+    private const string TRANSITION = "Sounds/KartEffects/screen_transition";
+    private AudioClip transition;
+    private AudioSource source;
+    private bool pressed;
 
-            SceneManager.LoadScene(prevScene);
+
+    void Start ()
+    {
+        pressed = false;
+        transition = (AudioClip)Resources.Load(TRANSITION);
+        source = GameObject.Find("Sound").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (SimpleInput.GetButton("Cancel") && !pressed) {
+            pressed = true;
+            StartCoroutine(Transition());
         }
 	}
+
+    private IEnumerator Transition()
+    {
+        source.clip = transition;
+        source.loop = false;
+        pressed = true;
+        source.Play();
+        yield return new WaitWhile(() => source.isPlaying);
+        var prevScene = SceneManager.GetActiveScene().buildIndex - 1;
+        if (prevScene == 0)
+        {
+            Destroy(GameObject.Find("SceneGenerator"));
+        }
+        SceneManager.LoadScene(prevScene);
+    }
 }
--- Assets/Scripts/Menus/SpudRunMenu.cs
+++ Assets/Scripts/Menus/SpudRunMenu.cs
@@ -7,22 +7,51 @@ using UnityEngine.EventSystems;
 
 public class SpudRunMenu : MonoBehaviour {
 
+    private const string TRANSITION = "Sounds/KartEffects/screen_transition";
+    private AudioClip transition;
+    private AudioSource source;
+    private bool pressed;
+
     public Button Arena1;
+    private GameObject levelSelection;
 
     private SceneGenerator sceneGenerator;
 
     void Start() {
-        EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(GameObject.Find("Arena 1"));
+        pressed = false;
+        transition = (AudioClip)Resources.Load(TRANSITION);
+        source = GameObject.Find("Sound").GetComponent<AudioSource>();
 
+        EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(GameObject.Find("Arena 1"));
+        levelSelection = GameObject.Find("LevelSelectionText");
         sceneGenerator = GameObject.Find("SceneGenerator").GetComponent<SceneGenerator>();
+    }
+
+    void Update()
+    {
+        if (pressed)
+        {
+            Arena1.transform.Translate(Vector3.left * 800.0f * Time.deltaTime);
+            levelSelection.transform.Translate(Vector3.left * 800.0f * Time.deltaTime);
+        }
     }
 
     public void Arena1Press() {
         //sceneGenerator.LevelName = "Arena1.xml";
-        GoToNextMenu();
+        StartCoroutine(Transition());
     }
 
     private void GoToNextMenu() {
         SceneManager.LoadScene("SelectionMenu");
+    }
+
+    private IEnumerator Transition()
+    {
+        source.clip = transition;
+        source.loop = false;
+        pressed = true;
+        source.Play();
+        yield return new WaitWhile(() => source.isPlaying);
+        GoToNextMenu();
     }
 }
