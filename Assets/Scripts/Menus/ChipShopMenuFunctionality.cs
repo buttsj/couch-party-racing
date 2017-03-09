48abc5a0812c2331b57649bf7b103f45e3a4a6b2
Jack Butts <butts.102@osu.edu>
2017-03-09 18:31:18 -0500

    added unique colors to Chip Shop

---
+++ Assets/Scripts/Menus/ChipShopMenuFunctionality.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChipShopMenuFunctionality : MonoBehaviour {
+
+    private WhiteFadeUniversal fader;
+
+    void Awake()
+    {
+        GameObject fadeObject = new GameObject();
+        fadeObject.name = "Fader";
+        fadeObject.transform.SetParent(transform);
+        fadeObject.SetActive(true);
+        fader = fadeObject.AddComponent<WhiteFadeUniversal>();
+        fader.BeginNewScene("Sound");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SimpleInput.GetButtonDown("Cancel"))
+        {
+            StartCoroutine(LeaveScene());
+        }
+    }
+
+    private IEnumerator LeaveScene()
+    {
+        fader.SceneSwitch();
+        while (!fader.Faded)
+            yield return null;
+        SceneManager.LoadScene(0);
+    }
+}
