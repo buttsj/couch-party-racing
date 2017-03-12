241fccb78c76dad0d3f9b49221e47c3e6004cabd
Jack Butts <butts.102@osu.edu>
2017-03-11 21:47:37 -0500

    started Chips with PlayerPrefs

---
+++ Assets/Scripts/Account/AccountManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class AccountManager : MonoBehaviour {
+    
+    private int chips;
+    public int CurrentChips { get { return chips; } set { chips = value; } }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+        // load account info
+        if (PlayerPrefs.HasKey("chips")){
+            chips = PlayerPrefs.GetInt("chips");
+        }
+        else
+        {
+            // new account, set PlayerPref
+            chips = 0;
+            PlayerPrefs.SetInt("chips", chips);
+        }
+        Debug.Log("Current chips: " + chips);
+	}
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("chips", chips); // save chips
+    }
+
+}
