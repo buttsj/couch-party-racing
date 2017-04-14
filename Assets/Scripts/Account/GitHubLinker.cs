bc71c280bb37552e42fe39151d78645497fe332c
John (Jack) Butts <butts.102@osu.edu>
2017-04-14 01:13:48 -0400

    github button works now

---
+++ Assets/Scripts/Account/GitHubLinker.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GitHubLinker : MonoBehaviour {
+
+	public void OnClick()
+    {
+        Application.OpenURL("https://github.com/buttsj/couch-party-racing");
+    }
+}
